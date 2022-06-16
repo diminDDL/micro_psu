c8c7da95f0400e5c44448d591bbc300a27837643
diminDDL <dimin21590@gmail.com>
2022-06-16 21:54:29 +0200

    added power supply identification

--- Micro_PSU/src/main.cpp
+++ Micro_PSU/src/main.cpp
@@ -41,32 +41,117 @@ void displayUpdate(void){
   lastUpdate = millis();
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(toRealVolt(PD_UFP.get_voltage(), true));
+  lcd.print(toRealVolt(PD_UFP.get_voltage(), isPPS));
   //Serial1.print((float)((PD_UFP.get_voltage()-0.01)/50));
   lcd.print("V");
   lcd.setCursor(0, 1);
-  lcd.print(toRealAmp(PD_UFP.get_current(), true));
+  lcd.print(toRealAmp(PD_UFP.get_current(), isPPS));
   lcd.print("A");
 }
 
-void testPSU(){
+void debugPulse(){
+  digitalWrite(9, HIGH);
+  delay(1);
+  digitalWrite(9, LOW);
+}
 
+void testPSU(){
+  // test PPS
+  PD_UFP.init_PPS(PPS_V(5.0), PPS_A(1.0), PD_POWER_OPTION_MAX_5V);
+  unsigned long testMillis = millis();
+  while(millis() - testMillis < 1000){
+    PD_UFP.run();
+    if (PD_UFP.is_PPS_ready()) {
+      isPPS = true;
+      break;
+    }
+  }
+  // if PPS is available then test max voltage and current
+  if(isPPS){
+    // check current
+    for (uint8_t i = 0; i <= PPS_A(5.0); i+=PPS_A(0.05)) {
+      if(PD_UFP.set_PPS(PPS_V(5.0), i)){
+        while(PD_UFP.is_ps_transition()){
+          PD_UFP.run();
+        }
+        if (PD_UFP.is_PPS_ready()) {
+          maxCurrent = toRealAmp(PD_UFP.get_current(), isPPS);
+        }
+      }
+    }
+
+    // check voltage
+    for (uint16_t i = PPS_V(5.0); i <= PPS_V(21.0); i+=PPS_V(1.00)) {
+      if(PD_UFP.set_PPS(i, PPS_A(maxCurrent))){
+        while(PD_UFP.is_ps_transition()){
+          PD_UFP.run();
+        }
+        if (PD_UFP.is_PPS_ready()) {
+          maxVoltage = toRealVolt(PD_UFP.get_voltage(), isPPS);
+        }
+      }
+    }
+     
+  }else{
+    // check voltage
+    PD_UFP.set_power_option(PD_POWER_OPTION_MAX_VOLTAGE);
+    while(PD_UFP.is_ps_transition()){
+      PD_UFP.run();
+    }      
+    maxVoltage = toRealVolt(PD_UFP.get_voltage(), isPPS);  
+    
+    // check current
+    PD_UFP.set_power_option(PD_POWER_OPTION_MAX_CURRENT);
+    while(PD_UFP.is_ps_transition()){
+      PD_UFP.run();
+    }
+    maxCurrent = toRealAmp(PD_UFP.get_current(), isPPS);
+  }
 }
 
 void setup() {
   pinMode(encoderBtnPin, INPUT_PULLUP);
   pinMode(1, OUTPUT);
+  pinMode(9, OUTPUT);
   Serial1.begin(115200);
   // while(!Serial1) {
   //   ;
   // }
   // Serial1.println("Encoder Test:");
   Wire.begin();
-  PD_UFP.init_PPS(PPS_V(5.31), PPS_A(2.5));
+  //PD_UFP.init_PPS(PPS_V(5.31), PPS_A(2.5));
   //PD_UFP.init(PD_POWER_OPTION_MAX_20V);
+
+
+  // PD_UFP.clock_prescale_set(2);
+  // clock_prescale_set(clock_div_2);
+
+  //PD_UFP.init_PPS(PPS_V(5.3), PPS_A(1.0));
   PD_UFP.clock_prescale_set(2);
   clock_prescale_set(clock_div_2);
+
   lcd.begin(8, 2);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Testing");
+  lcd.setCursor(0, 1);
+  lcd.print("PSU...");
+  testPSU();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(maxVoltage);
+  //Serial1.print((float)((PD_UFP.get_voltage()-0.01)/50));
+  lcd.print("V");
+  lcd.setCursor(0, 1);
+  lcd.print(maxCurrent);
+  lcd.print("A");
+  if(isPPS){
+    lcd.print("PPS");
+  }
+  delay(5000);
+
+  PD_UFP.set_PPS(PPS_V(5.0), PPS_A(2.8));
 }
 
 long position = -999;
@@ -97,6 +182,13 @@ void loop() {
     PD_UFP.blink_led(400);              // blink LED
   }
 
+  if (PD_UFP.is_PPS_ready()) {
+    isPPS = true;
+    digitalWrite(9, HIGH);
+  }else{
+    isPPS = false;
+    digitalWrite(9, LOW);
+  }
   // if (PD_UFP.is_power_ready()) { 
   //   if (PD_UFP.get_voltage() == PD_V(20.0) && PD_UFP.get_current() >= PD_A(1.5)) {
   //     PD_UFP.set_output(1);   // Turn on load switch 
